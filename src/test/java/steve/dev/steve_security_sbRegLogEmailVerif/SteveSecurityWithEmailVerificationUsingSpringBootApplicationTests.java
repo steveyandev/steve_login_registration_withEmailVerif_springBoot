64399e7f5f0ba074ea4865b755eaacf808bd1eba
steve desire <stevedesiredev@gmail.com>
2023-10-11 00:26:28 +0200

    first commit of spring boot using spring security. The application manage login and registration with email verification

---
+++ src/test/java/steve/dev/steve_security_sbRegLogEmailVerif/SteveSecurityWithEmailVerificationUsingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package steve.dev.steve_security_sbRegLogEmailVerif;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteveSecurityWithEmailVerificationUsingSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
