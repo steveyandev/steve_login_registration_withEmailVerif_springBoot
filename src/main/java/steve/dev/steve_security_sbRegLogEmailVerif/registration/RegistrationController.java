d087face2f2be320695f4147c8cd56ffde49230f
steve desire <stevedesiredev@gmail.com>
2023-10-13 16:46:46 +0200

    implementation of the registration but not complete : controller, registration request model, methode register in registration service without implementation yet.

--- src/main/java/steve/dev/steve_security_sbRegLogEmailVerif/registration/RegistrationController.java
+++ src/main/java/steve/dev/steve_security_sbRegLogEmailVerif/registration/RegistrationController.java
@@ -1,4 +1,19 @@
 package steve.dev.steve_security_sbRegLogEmailVerif.registration;
 
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import steve.dev.steve_security_sbRegLogEmailVerif.services.RegistrationService;
+
+@RestController
+@RequestMapping(path = "api/v1/registration")
+@AllArgsConstructor
 public class RegistrationController {
+
+    private RegistrationService registrationService;
+    public String register(@RequestBody RegistrationRequest request){
+        return registrationService.register(request);
+    }
 }
---
+++ src/main/java/steve/dev/steve_security_sbRegLogEmailVerif/services/RegistrationService.java
@@ -0,0 +1,11 @@
+package steve.dev.steve_security_sbRegLogEmailVerif.services;
+
+import org.springframework.stereotype.Service;
+import steve.dev.steve_security_sbRegLogEmailVerif.registration.RegistrationRequest;
+
+@Service
+public class RegistrationService {
+    public String register(RegistrationRequest request) {
+        return null;
+    }
+}
