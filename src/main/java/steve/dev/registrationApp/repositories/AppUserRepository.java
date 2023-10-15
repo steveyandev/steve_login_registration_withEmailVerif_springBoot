255d9811e765b1a836cdec01d5db20234527cc35
steve desire <stevedesiredev@gmail.com>
2023-10-15 02:09:59 +0200

    fist test of the app with postman test of the controller and it's working. now for the following we will implement the logic, service for registration logic

--- src/main/java/steve/dev/registrationApp/repositories/AppUserRepository.java
+++ src/main/java/steve/dev/registrationApp/repositories/AppUserRepository.java
@@ -1,5 +1,7 @@
 package steve.dev.registrationApp.repositories;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import steve.dev.registrationApp.appUser.AppUser;
@@ -8,6 +10,6 @@ import java.util.Optional;
 
 @Repository
 @Transactional(readOnly = true)
-public interface AppUserRepository {
+public interface AppUserRepository extends JpaRepository<AppUser,Long> {
     Optional<AppUser> findByEmail(String email);
 }
--- src/main/java/steve/dev/registrationApp/security/PasswordEncoder.java
+++ src/main/java/steve/dev/registrationApp/security/PasswordEncoder.java
@@ -3,9 +3,10 @@ package steve.dev.registrationApp.security;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@Configurable
+@Configuration
 public class PasswordEncoder {
 
     @Bean
